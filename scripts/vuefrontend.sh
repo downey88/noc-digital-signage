f8210f7658342c9b102fe135241c4af035f778c8
Donny Tri <donny.tri88@gmail.com>
2022-07-12 11:36:22 +0700

    Update vuefrontend.sh

--- scripts/vuefrontend.sh
+++ scripts/vuefrontend.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 npm run build
-cd /home/ndsignage/noc-digital-signage
+npm run test:unit
+
+cd /home/ndsignage/noc-digital-signage/_work/noc-digital-signage/noc-digital-signage
+cp dist/index.html /var/www/html
+cp dist/favicon.ico /var/www/html
+cp -R dist/css /var/www/html
+cp -R dist/img /var/www/html
+cp -R dist/js /var/www/html
+
